580daf496480ad811f14ce995886d21588b6ff8c
nikita-yfh <nikita.yfh@gmail.com>
2023-10-21 10:08:31 +0300

    header

--- vm/common/version.c
+++ vm/common/version.c
@@ -1,6 +1,7 @@
 #include "version.h"
 
 #include <stdio.h>
+#include <stdint.h>
 
 #include "colors.h"
 
@@ -19,4 +20,25 @@ void printVersion(const char *programName) {
 	printf(COLOR_GREEN"%s"COLOR_NONE, nameArt);
 	printf("This is "COLOR_BLUE"%s"COLOR_NONE" of "COLOR_MAGENTA PROGRAM COLOR_NONE
 		" v"VERSION" by "COLOR_GREEN"NIKITA"COLOR_NONE "(C)\n\n", programName);
+}
+
+struct Header {
+	uint32_t signature;
+	uint32_t version;
+};
+
+void writeHeader(FILE *file) {
+	struct Header header = {SIGNATURE, VERSION_CODE};
+	fwrite(&header, sizeof(header), 1, file);
+}
+
+int checkHeader(FILE *file) {
+	struct Header header = {};
+	if(!fread(&header, sizeof(header), 1, file))
+		return -1;
+	if(header.signature != SIGNATURE)
+		return -1;
+	if(header.version < MIN_VERSION_CODE || header.version > VERSION_CODE)
+		return -1;
+	return 0;
 }
--- vm/common/version.h
+++ vm/common/version.h
@@ -1,10 +1,19 @@
 #ifndef VERSION_H
 #define VERSION_H
 
+#include <stdio.h>
+
 #define VERSION "1.0.0"
+
 #define VERSION_CODE 1
+#define MIN_VERSION_CODE 1
+
+#define SIGNATURE 0x6866796E
 
 void printVersion(const char *programName);
+
+void writeHeader(FILE *file);
+int checkHeader(FILE *file);
 
 #endif
 
