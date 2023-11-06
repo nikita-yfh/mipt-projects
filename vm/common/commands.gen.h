9c50888c25f0bd66e263c5432abcbd7cb755f8f2
nikita-yfh <nikita.yfh@gmail.com>
2023-11-06 04:35:44 +0300

    Add commands.gen.h

---
+++ vm/common/commands.gen.h
@@ -0,0 +1,166 @@
+#define DEF_COMMAND_MATH(name, number, op, checkZero)	\
+	DEF_COMMAND(name, number, false, {					\
+		stackValue_t a = 0, b = 0, c = 0;				\
+		STACK_POP(processor, &b);						\
+		STACK_POP(processor, &a);						\
+		if(checkZero && b == 0)							\
+			return EXEC_MATH_ERROR;						\
+		c = a op b;										\
+		STACK_PUSH(processor, c);						\
+		return EXEC_OK;									\
+	})
+
+#define DEF_COMMAND_FMATH(name, number, op)				\
+	DEF_COMMAND(name, number, false, {					\
+		float a = NAN, b = NAN, c = NAN;				\
+		STACK_POP(processor, (stackValue_t*) &b);		\
+		STACK_POP(processor, (stackValue_t*) &a);		\
+		STACK_PUSH(processor, *((stackValue_t*) &c));	\
+		return EXEC_OK;									\
+	})
+
+#define DEF_JUMP(name, number, op)						\
+	DEF_COMMAND(name, number, false, {					\
+		arg_t *address = NULL;							\
+		GET_ARGUMENT(processor, instruction,			\
+				DIR_IN, &address);						\
+		stackValue_t arg = 0;							\
+		STACK_POP(processor, &arg);						\
+		if(arg op)										\
+			processor->pc = *address;					\
+		return EXEC_OK;									\
+	})
+
+DEF_COMMAND("nop", 0x01, false, {
+	return EXEC_OK;
+})
+
+DEF_COMMAND("hlt", 0x02, false, {
+	processor->pc = PC_HLT;
+	return EXEC_END_PROGRAM;
+})
+
+DEF_COMMAND("push", 0x03, true, {
+	arg_t *arg = NULL;
+	GET_ARGUMENT(processor, instruction, DIR_IN, &arg);
+
+	STACK_PUSH(processor, (stackValue_t) *arg);
+	return EXEC_OK;
+})
+
+DEF_COMMAND("pop", 0x04, true, {
+	stackValue_t var = 0;
+	STACK_POP(processor, &var);
+
+	arg_t *arg = NULL;
+	GET_ARGUMENT(processor, instruction, DIR_OUT, &arg);
+
+	*arg = (arg_t) var;
+	return EXEC_OK;
+})
+
+DEF_COMMAND_MATH("add", 0x05, +, false)
+DEF_COMMAND_MATH("sub", 0x06, -, false)
+DEF_COMMAND_MATH("mul", 0x07, *, false)
+DEF_COMMAND_MATH("div", 0x08, /, true)
+DEF_COMMAND_MATH("mod", 0x09, %, true)
+
+DEF_COMMAND("in", 0x0A, false, {
+	stackValue_t a = 0;
+	scanf("%d", &a);
+	STACK_PUSH(processor, a);
+	return EXEC_OK;
+})
+
+DEF_COMMAND("out", 0x0A, false, {
+	stackValue_t a = 0;
+	STACK_POP(processor, &a);
+	printf("%d\n", a);
+	return EXEC_OK;
+})
+
+DEF_COMMAND_FMATH("fadd", 0x0B, +);
+DEF_COMMAND_FMATH("fsub", 0x0C, -);
+DEF_COMMAND_FMATH("fmul", 0x0D, *);
+DEF_COMMAND_FMATH("fdiv", 0x0E, /);
+
+DEF_COMMAND("sqrt", 0x0F, false, {
+	float a = NAN;
+	STACK_POP(processor, (stackValue_t*) &a);
+	float b = sqrtf(a);
+	STACK_PUSH(processor, *((stackValue_t*) &b));
+	return EXEC_OK;
+})
+
+DEF_COMMAND("fin", 0x10, false, {
+	float a = NAN;
+	scanf("%f", &a);
+	STACK_PUSH(processor, *((stackValue_t*) &a));
+	return EXEC_OK;
+})
+
+DEF_COMMAND("fout", 0x11, false, {
+	float a = 0;
+	STACK_POP(processor, (stackValue_t*) &a);
+	printf("%g\n", a);
+	return EXEC_OK;
+})
+
+DEF_COMMAND("df", 0x12, false, {
+	stackValue_t d = 0;
+	STACK_POP(processor, &d);
+	float f = (float) d;
+	STACK_PUSH(processor, *((stackValue_t*) &f));
+	return EXEC_OK;
+})
+
+DEF_COMMAND("fd", 0x13, false, {
+	float f = NAN;
+	STACK_POP(processor, (stackValue_t*) &f);
+	int d = (int) f;
+	STACK_PUSH(processor, d);
+	return EXEC_OK;
+})
+
+DEF_COMMAND("jmp", 0x14, true, {
+	arg_t *address = NULL;
+	GET_ARGUMENT(processor, instruction, DIR_IN, &address);
+	
+	processor->pc = *address;
+	return EXEC_OK;
+})
+
+DEF_JUMP("jmp", 0x15, || true)
+DEF_JUMP("jge", 0x16, >= 0)
+DEF_JUMP("jg",  0x17, >  0)
+DEF_JUMP("jle", 0x18, <= 0)
+DEF_JUMP("jl",  0x19, <  0)
+DEF_JUMP("je",  0x1A, == 0)
+DEF_JUMP("jne", 0x1B, != 0)
+
+
+DEF_COMMAND("call", 0x1C, true, {
+	arg_t *address = NULL;
+	GET_ARGUMENT(processor, instruction, DIR_IN, &address);
+
+	if(stackPush(&processor->callStack, (stackValue_t) processor->pc))
+		return EXEC_STACK_OVERFLOW;
+	processor->pc = *address;
+	return EXEC_OK;
+})
+
+DEF_COMMAND("ret", 0x1D, false, {
+	int32_t jump = 0;
+	if(stackPop(&processor->callStack, &jump))
+		return EXEC_STACK_EMPTY;
+
+	processor->pc = (arg_t) jump;
+	return EXEC_OK;
+})
+
+DEF_COMMAND("upd", 0x1E, false, {
+	if(processor->videoEnable)
+		graphicsUpdate();
+	return EXEC_OK;
+})
+
